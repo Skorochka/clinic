1fa282f9312c8df8743fa0c11f52ad64b274ccb1
Skorochka <79852838+Skorochka@users.noreply.github.com>
2021-08-30 23:15:24 +0300

    Сохраняет последние изменения

--- src/index.js
+++ src/index.js
@@ -1,21 +1,21 @@
 
 
-// const refs = {
-//     dataMenuBtn: document.querySelector('#modal-btn'),
-//     dataMenu: document.querySelector('[data-menu]')
-// }
+const refs = {
+    dataMenuBtn: document.querySelector('#modal-btn'),
+    dataMenu: document.querySelector('[data-menu]')
+}
 
-// refs.dataMenuBtn.addEventListener('click', onModalOpen)
+refs.dataMenuBtn.addEventListener('click', onModalOpen)
 
-// function onModalOpen(e) {
-//     const expanded = refs.dataMenuBtn.getAttribute('aria-expanded') === 'true' || false
-//     console.log(expanded)
+function onModalOpen(e) {
+    const expanded = refs.dataMenuBtn.getAttribute('aria-expanded') === 'true' || false
+    console.log(expanded)
 
-//     refs.dataMenuBtn.classList.toggle('is-open')
-//     refs.dataMenuBtn.setAttribute('aria-expanded', !expanded)
+    refs.dataMenuBtn.classList.toggle('is-open')
+    refs.dataMenuBtn.setAttribute('aria-expanded', !expanded)
 
-//     refs.dataMenu.classList.toggle('is-open')
-// }
+    refs.dataMenu.classList.toggle('is-open')
+}
 
 
 // $('.owl-carousel').owlCarousel({
