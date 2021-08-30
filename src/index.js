8190059290434157510deabfb5cc6185bf6e5960
Skorochka <79852838+Skorochka@users.noreply.github.com>
2021-08-30 22:53:41 +0300

    Добавляет слайдер в hero

--- src/index.js
+++ src/index.js
@@ -1,16 +1,38 @@
-const refs = {
-    dataMenuBtn: document.querySelector('#modal-btn'),
-    dataMenu: document.querySelector('[data-menu]')
-}
 
-refs.dataMenuBtn.addEventListener('click', onModalOpen)
 
-function onModalOpen(e) {
-    const expanded = refs.dataMenuBtn.getAttribute('aria-expanded') === 'true' || false
-    console.log(expanded)
+// const refs = {
+//     dataMenuBtn: document.querySelector('#modal-btn'),
+//     dataMenu: document.querySelector('[data-menu]')
+// }
 
-    refs.dataMenuBtn.classList.toggle('is-open')
-    refs.dataMenuBtn.setAttribute('aria-expanded', !expanded)
+// refs.dataMenuBtn.addEventListener('click', onModalOpen)
 
-    refs.dataMenu.classList.toggle('is-open')
-}
+// function onModalOpen(e) {
+//     const expanded = refs.dataMenuBtn.getAttribute('aria-expanded') === 'true' || false
+//     console.log(expanded)
+
+//     refs.dataMenuBtn.classList.toggle('is-open')
+//     refs.dataMenuBtn.setAttribute('aria-expanded', !expanded)
+
+//     refs.dataMenu.classList.toggle('is-open')
+// }
+
+
+// $('.owl-carousel').owlCarousel({
+//     loop:true,
+//     margin:10,
+//     nav: true,
+
+//     responsive:{
+//         0:{
+//             items:1
+//         },
+//         600:{
+//             items:3
+//         },
+//         1000:{
+//             items:5
+//         }
+//     }
+// })
+
---
+++ src/js/swipe.js
@@ -0,0 +1,37 @@
+let offset = 0;
+let width 
+const sliderLine = document.querySelector('.slider-line')
+const btnLeft = document.querySelector('.hero__content-btn-left')
+
+const btnRigth = document.querySelector('.hero__content-btn-right')
+
+
+btnLeft.addEventListener('click', onBtnLeftClick)
+ btnRigth.addEventListener('click', onBtnRightClick)
+
+
+width = document.querySelector('.slider').offsetWidth;
+console.log(width)
+ 
+
+
+function onBtnRightClick() {
+   
+    offset = offset + width;
+    if (offset > (width * 4)) {
+        offset = 0
+    }
+    sliderLine.style.left = -offset + 'px';
+
+}
+
+function onBtnLeftClick() {
+   
+
+    offset = offset - width;
+    if (offset < 0) {
+        offset = (width * 4)
+    }
+    sliderLine.style.left = -offset + 'px';
+}
+
